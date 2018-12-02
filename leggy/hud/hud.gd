5fda33a08b2b83d1c447d90c2bedb2704cc1d2a7
Lee <4343398+organicpencil@users.noreply.github.com>
2018-12-02 04:43:46 +0000

    Message improvements (#7)
    * Weird officer, better font, typewriter effect
    
    * Fix 2nd mesage not showing

--- leggy/hud/hud.gd
+++ leggy/hud/hud.gd
@@ -1,17 +1,13 @@
 extends Control
 
 signal game_over
+var target_message = ""
+var current_message_position = 0
 
 func _ready():
-	set_process(false)
-
 	$Timer.connect("timeout", self, "handle_timeout")
 
-	# Temp
-	#start()
-
 func handle_timeout():
-	set_process(false)
 	$TimeLabel.text = "00"
 
 	emit_signal("game_over")
@@ -24,7 +20,16 @@ func start():
 	set_process(true)
 
 func message(text : String):
-	$MessageLabel.text = text
+	current_message_position = 0
+	target_message = text
+	$MessageLabel.text = ""
+	$Officer/AnimationPlayer.play("talk")
 
 func _process(delta):
 	$TimeLabel.text = ("%.0f" % [$Timer.time_left]).pad_zeros(2)
+
+	if current_message_position < target_message.length():
+		$MessageLabel.text += target_message[current_message_position]
+		current_message_position += 1
+		if current_message_position == target_message.length():
+			$Officer/AnimationPlayer.stop()
