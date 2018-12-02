f45dd751a57062756eecf4288460748db0aaf10f
Austin Irvine <aa1irvine@gmail.com>
2018-12-02 13:29:59 -0600

    Leg Cuts Off (#12)
    * Leg Cuts Off
    
    * Align the leg with saw

--- leggy/Assets/sawswipe.gd
+++ leggy/Assets/sawswipe.gd
@@ -8,11 +8,11 @@ func _on_SwipeDetector_swipe_started( partial_gesture ):
 func _on_SwipeDetector_swipe_updated( partial_gesture ):
 	var point = partial_gesture.last_point()
 	var player = get_parent().get_node(get_parent().charOBJ)
-	player._saw_loc(point)
+	player._saw_loc(point, partial_gesture.get_direction())
 
 func _on_SwipeDetector_swipe_updated_with_delta( partial_gesture, delta ):
 	var player = get_parent().get_node(get_parent().charOBJ)
-	#player._saw_loc(delta)
+	player._saw_pitch(delta)
 
 func _on_SwipeDetector_swipe_ended( gesture ):
 	var player = get_parent().get_node(get_parent().charOBJ)
