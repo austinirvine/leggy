5efb443da46da111bf8b910758098200a8748a3b
Lee Pugh <4343398+organicpencil@users.noreply.github.com>
2018-12-01 22:01:22 -0600

    Weird officer, better font, typewriter effect

--- leggy/hud/hud.gd
+++ leggy/hud/hud.gd
@@ -1,14 +1,13 @@
 extends Control
 
 signal game_over
+var target_message = ""
+var current_message_position = 0
 
 func _ready():
 	set_process(false)
 
 	$Timer.connect("timeout", self, "handle_timeout")
-
-	# Temp
-	#start()
 
 func handle_timeout():
 	set_process(false)
@@ -24,7 +23,15 @@ func start():
 	set_process(true)
 
 func message(text : String):
-	$MessageLabel.text = text
+	$MessageLabel.text = ""
+	target_message = text
+	$Officer/AnimationPlayer.play("talk")
 
 func _process(delta):
 	$TimeLabel.text = ("%.0f" % [$Timer.time_left]).pad_zeros(2)
+
+	if current_message_position < target_message.length():
+		$MessageLabel.text += target_message[current_message_position]
+		current_message_position += 1
+		if current_message_position == target_message.length():
+			$Officer/AnimationPlayer.stop()
--- leggy/hud/test.gd
+++ leggy/hud/test.gd
@@ -3,7 +3,8 @@ extends Node
 func _ready():
 	$HUD.connect("game_over", self, "handle_game_over")
 	$HUD.start()
-	$HUD.message("Hurry up and chop that leg off/n(swipe the saw left and right)")
+	$HUD.message("Hurry up and get that leg off, soldier!\n(swipe the saw left and right)")
 
 func handle_game_over():
 	print("Game over")
+	$HUD.message("GODDAMNIT JENKINS!\n(you died)")
