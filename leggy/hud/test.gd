b52b43d792d5f1d56c83669568c5d4c07207c220
Lee Pugh <4343398+organicpencil@users.noreply.github.com>
2018-12-01 20:41:16 -0600

    Simple message function

--- leggy/hud/test.gd
+++ leggy/hud/test.gd
@@ -3,6 +3,7 @@ extends Node
 func _ready():
 	$HUD.connect("game_over", self, "handle_game_over")
 	$HUD.start()
+	$HUD.message("Hurry up and chop that leg off/n(swipe the saw left and right)")
 
 func handle_game_over():
 	print("Game over")
