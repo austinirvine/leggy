320c48c8196c05dbf05399660e6a295d67236ca1
austinirvine <aa1irvine@gmail.com>
2018-12-02 13:17:46 -0600

    Leg Cuts Off

--- leggy/Assets/Character.gd
+++ leggy/Assets/Character.gd
@@ -6,10 +6,18 @@ var recent_point;
 
 # Progress
 var progress = 0
+var first_move = 0
+var cut_threshold = false
 # Accelerometer
 #var acc_threshold = 1
 # Gyroscope
 #var gyr_threshold = 1
+
+# Audio
+onready var audio_player_in = get_node("AudioStreamPlayer3D_in")
+onready var audio_player_out = get_node("AudioStreamPlayer3D_out")
+#var saw_in = preload("res://SawIn_1.wav")
+#var saw_out = preload("res://SawOut_1.wav")
 
 # Operating System
 var op_sys
@@ -30,21 +38,52 @@ func _ready():
 
 func _swipe_signal(dir):
 	print(dir)
-	_move_saw()
 
-func _saw_loc(point):
-	if (progress == 0):
+func _saw_loc(point, dir):
+	print(dir)
+	if (first_move == 0):
 		last_point = point
+		first_move = 1
 		return
 	recent_point = point
 	var saw = get_node( "Saw" )
 	var deltaZ = (recent_point.y - last_point.y) / 100
-	print(deltaZ)
 	saw.translate( Vector3 ( 0.0, 0.0, deltaZ))
 	last_point = point
+	_make_cut(saw, last_point.y)
 
-func _move_saw():
-	var saw = get_node( "Saw" )
-	saw.translate( Vector3 ( 0.0, -.25, 0.0 ))
-	progress += 1
-	print(progress)
+func _make_cut(saw, zLoc):
+	#print(zLoc)
+	if cut_threshold == false && zLoc < 200:
+		cut_threshold = true
+		audio_player_out._set_playing(true)
+		audio_player_in._set_playing(false)
+		pass
+	elif cut_threshold == true && zLoc > 300:
+		cut_threshold = false
+		audio_player_out._set_playing(false)
+		audio_player_in._set_playing(true)
+		saw.translate( Vector3 ( 0.0, -0.15, 0.0 ))
+		progress += 1
+
+	#leg change to ragdoll
+	if (progress == 15) :
+		var leg = get_node("Leg")
+		var calf_withrag = leg.get_node("calf-withrag")
+		var calf_norag = leg.get_node("calf-norag")
+
+		leg.remove_child( calf_norag )
+		calf_withrag.set_visible(true)
+		calf_withrag.pause_mode = Node.PAUSE_MODE_INHERIT
+		pass
+	#print(progress)
+
+func _saw_pitch(delta) :
+	var pitch_value = audio_player_in.get_pitch_scale() * delta / 10
+	audio_player_in.set_pitch_scale(delta + audio_player_in.get_pitch_scale())
+	audio_player_out.set_pitch_scale(delta + audio_player_out.get_pitch_scale())
+
+	if(delta > .1) :
+		audio_player_in.set_pitch_scale(1)
+		audio_player_out.set_pitch_scale(1)
+	pass
