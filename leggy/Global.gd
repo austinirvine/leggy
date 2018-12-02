79ccc66501d3fb91a119c284c42c8879c59659b3
Lee Pugh <4343398+organicpencil@users.noreply.github.com>
2018-12-02 02:39:26 -0600

    Press escape to load main menu

--- leggy/Global.gd
+++ leggy/Global.gd
@@ -1,5 +1,10 @@
 extends Node
 
+const MENU_SCENE = preload("res://menu/menu.tscn")
+
 func _input(event):
 	if event.is_action_pressed("toggle_fullscreen"):
 		OS.window_fullscreen = !OS.window_fullscreen
+
+	if event.is_action_pressed("quit"):
+		get_tree().change_scene_to(MENU_SCENE)
