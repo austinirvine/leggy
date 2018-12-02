80f3046a5bd0f31db22254197b22210d76e3db86
Austin Irvine <aa1irvine@gmail.com>
2018-12-01 22:58:43 -0600

    Swipe to Saw (#8)

--- leggy/Assets/Character.gd
+++ leggy/Assets/Character.gd
@@ -1,13 +1,50 @@
 extends Spatial
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# Saw Locations
+var last_point;
+var recent_point;
+
+# Progress
+var progress = 0
+# Accelerometer
+#var acc_threshold = 1
+# Gyroscope
+#var gyr_threshold = 1
+
+# Operating System
+var op_sys
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print("Starting")
+
+	last_point = Vector2(0.0, 0.0);
+	recent_point = Vector2(0.0, 0.0);
+
+	op_sys = OS.get_name()
+	if op_sys == "Android":
+		pass
+	elif op_sys == "iOS":
+		pass
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _swipe_signal(dir):
+	print(dir)
+	_move_saw()
+
+func _saw_loc(point):
+	if (progress == 0):
+		last_point = point
+		return
+	recent_point = point
+	var saw = get_node( "Saw" )
+	var deltaZ = (recent_point.y - last_point.y) / 100
+	print(deltaZ)
+	saw.translate( Vector3 ( 0.0, 0.0, deltaZ))
+	last_point = point
+
+func _move_saw():
+	var saw = get_node( "Saw" )
+	saw.translate( Vector3 ( 0.0, -.25, 0.0 ))
+	progress += 1
+	print(progress)
